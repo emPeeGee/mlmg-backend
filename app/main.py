b13d02b0e8964600caa4b9568757da9478d75273
emPeeGee <eempeegee@gmail.com>
2024-08-12 21:52:37 +0300

    ✨ feat: use lifecycle method of the fastapi to close the db connection

--- app/main.py
+++ app/main.py
@@ -2,14 +2,20 @@ import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
+from fastapi.concurrency import asynccontextmanager
+from core.models import db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app_param: FastAPI):
+    # startup
+    yield
+    # Shutdown
+    await db_helper.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix=settings.api.prefix)
 
-if __name__ == '__main__':
-    uvicorn.run(
-        "main:app",
-        host=settings.run.host,
-        port=settings.run.port,
-        reload=True
-    )
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
